022ec17aa18bb231dcde5ca7ed45c5383c0cd71c
basuabhirup <basu.abhirup27@gmail.com>
2024-05-22 12:56:51 +0530

    Add user profile page

--- network/views.py
+++ network/views.py
@@ -1,14 +1,14 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 import json
 from django.contrib.auth import get_user_model
 from django.http import JsonResponse
-from .models import Post
+from .models import Post, Follower
 
 from .models import User
 
@@ -107,3 +107,34 @@ def posts(request):
     })
   else:
     return JsonResponse({'error': 'Method not allowed'}, status=405)
+
+
+def profile(request, username):
+  # Get the user with the given username
+  user = get_object_or_404(User, username=username)
+
+  # Get all posts for the user
+  user_posts = Post.objects.filter(user=user).order_by('-timestamp')
+
+  # Get follower count for the user
+  follower_count = user.followers.count()
+
+  # Get following count for the user (optional)
+  following_count = user.following.count()  # Optional, depending on your model
+  
+  # Check if the user is viewing their own profile
+  show_follow_button = request.user != user and request.user.is_authenticated
+
+  # Check if current user is following the profile user (if logged in)
+  following = False
+  if show_follow_button:
+    following = Follower.objects.filter(follower=request.user, following=user).exists()
+
+  return render(request, 'network/profile.html', {
+      'profile_user': user,
+      'posts': user_posts,
+      'follower_count': follower_count,
+      'following_count': following_count,
+      'following': following,
+      'show_follow_button': show_follow_button
+  })
