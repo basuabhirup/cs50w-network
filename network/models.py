9ce9fed3462bbdfd8f6492c7d801dfe0eccd00ef
basuabhirup <basu.abhirup27@gmail.com>
2024-05-22 12:19:44 +0530

    Add Followers model and migrate data

--- network/models.py
+++ network/models.py
@@ -10,3 +10,10 @@ class Post(models.Model):
   content = models.TextField()
   timestamp = models.DateTimeField(auto_now_add=True)
   likes = models.PositiveIntegerField(default=0)
+
+class Follower(models.Model):
+  follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
+  following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers')
+
+  class Meta:
+    unique_together = ('follower', 'following')
